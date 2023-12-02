49d37fef981472ae914093d7eff34052d07b911f
Spirit <96442875+Spirit071@users.noreply.github.com>
2023-12-02 18:47:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+
+var sum = 0;
+
+async function fetchUrl(url, url2) {
+    try {
+        await fetch(url).then(res => res.headers.forEach(header => {
+             sum += header.length; console.log(sum)
+        }));
+        await fetch(url2).then(res => res.headers.forEach(header => {
+            sum += header.length; 
+            console.log(sum)
+        }))
+    } catch (error) {
+        console.error(`Error fetching ${url}: ${error.message}`);
+    }
+}
+
+function fetchPeriodically() {
+    fetchUrl('https://translateapi-x.onrender.com/', 'https://keepthatshitalive.onrender.com/');
+    setInterval(() => {
+        fetchUrl(urlToFetch);
+    }, 30000);
+}
+
+fetchPeriodically();
