2533dea0110a20fbb6c4de31d120c2ea3539ea15
Spirit <96442875+Spirit071@users.noreply.github.com>
2023-12-24 08:45:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,14 @@ var sum = 0;
 
 async function fetchUrl(url, url2) {
     try {
-        await fetch(url).then(res => res.headers.forEach(header => {
-             sum += header.length; console.log(sum)
-        }));
-        await fetch(url2).then(res => res.headers.forEach(header => {
-            sum += header.length; 
-            console.log(sum)
-        }))
+        await fetch(url)
+        await fetch(url2)
     } catch (error) {
         console.error(`Error fetching ${url}: ${error.message}`);
     }
 }
 
 function fetchPeriodically() {
-    fetchUrl('https://translateapi-x.onrender.com/', 'https://keepthatshitalive.onrender.com/');
     setInterval(() => {
         fetchUrl('https://translateapi-x.onrender.com/', 'https://keepthatshitalive.onrender.com/');
     }, 30000);
